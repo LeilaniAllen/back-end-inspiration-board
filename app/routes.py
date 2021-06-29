8a1801db584170ae057de5253e9fb23906ab9c42
mairandomness <mairakodama@gmail.com>
2021-06-29 09:55:46 -0700

    Add Blueprints and one to many relationship between models

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
+from app.models.board import Board
+
 
 # example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
